c8e26f7fa43335a8fad94867866a9e9822875a33
EcleraLera <borisowa.valerija2015@yandex.ru>
2023-11-10 19:26:35 +0300

    remember handler

--- js/script.js
+++ js/script.js
@@ -1,20 +1,57 @@
-function insert(num) {
-    document.form.textview.value = document.form.textview.value + num;
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let buttons = document.querySelectorAll("button");
+ 
+function remember(event) {
+    // console.log( typeof event.target.innerText)
+    let target = event.target.innerText;
+    if (target === 'RESET') {
+        firstNumber = '';
+    } else if (target === '.') {
+        firstNumber += target;
+    } else if (target === '+' || target === '-' || target === '/' || target === '*' ) {
+        operator += target;
+        operator.length = 1;
+    } else if (target === 'DEL') {
+        firstNumber = firstNumber.slice(0, -1);
+    } else { 
+        if (operator != '') {
+            secondNumber += target;
+            secondNumber.length <= 12; 
+        } else {
+            firstNumber += target;
+            secondNumber.length <= 12; 
+        }
+    }
+   
+    console.log(firstNumber);
+    console.log(operator);
+    console.log(secondNumber);
+
 }
 
-function reset() {
-    document.form.textview.value = "";   
-}
+ Array.from(buttons).forEach(button => {
+    button.addEventListener("click", remember)
+    }
+ )
 
-function del() {
-    let exp = document.form.textview.value;
-    document.form.textview.value = exp.substring(0,
-    exp.length-1);
-}
-
-function equal() {
-    let exp = document.form.textview.value;
-    if (exp) {
-        document.form.textview.value = eval(exp);
+function update(event) {
+    let target = event.target.innerText;
+    if (target === '+' || target === '-' || target === '/' || target === '*' || target === '=' || target === 'RESET') {
+        document.getElementById('rezult').innerHtml = '';
     }
 }
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener("click", update)
+    }
+ )
+ 
+ function calculate() {
+
+
+ }
+ 
+ button.addEventListener('click', calculate)
+
